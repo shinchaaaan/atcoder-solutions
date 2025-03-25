99102af981e8ac749e818b4e00a48b1ce8ca3a20
shinchaaaan <michiya.ayihcim@gmail.com>
2025-03-26 07:02:49 +0900

    Add ABC093 B

---
+++ beginner/abc093/b.py
@@ -0,0 +1,12 @@
+# ABC093 B
+
+A, B, K = map(int, input().split())
+
+if B - A + 1 < 2 * K:
+    for i in range(A, B + 1):
+        print(i)
+else:
+    for i in range(A, A + K):
+        print(i)
+    for i in range(B - K + 1, B + 1):
+        print(i)
